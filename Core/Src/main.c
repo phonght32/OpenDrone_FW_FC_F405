690db67c4b909c84a915aa7f4b9c0b92a6ae3905
phonght32 <thanhphongho1998@gmail.com>
2024-06-09 09:34:52 +0700

    use OpenDrone_FC_FW to init and run main process

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -33,36 +33,15 @@
 /* USER CODE END Includes */
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-#define FREQ_HZ_TO_TIME_US(x)       (1000000.0f/(x))
-#define TIME_US_TO_FREQ_HZ(x)       (1000000.0f/(x))
-
-#define IDX_TASK_500_HZ             0
-#define IDX_TASK_100_HZ             1
-#define IDX_TASK_50_HZ              2
-#define IDX_TASK_5_HZ               3
-#define NUM_OF_TASK                 4
-
-#define FREQ_500_HZ_TIME_US         FREQ_HZ_TO_TIME_US(500)
-#define FREQ_100_HZ_TIME_US         FREQ_HZ_TO_TIME_US(100)
-#define FREQ_50_HZ_TIME_US          FREQ_HZ_TO_TIME_US(50)
-#define FREQ_5_HZ_TIME_US           FREQ_HZ_TO_TIME_US(5)
 /* USER CODE END PTD */
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-// #define PRINT_ANGLE
 /* USER CODE END PD */
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 /* USER CODE END PM */
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN PV */
-#ifdef USE_SERIAL_DEBUG
-uint8_t log_buf[50];
-uint16_t task_freq[NUM_OF_TASK];
-#endif
-
-uint32_t last_time_us[NUM_OF_TASK] = {0};
-OpenDrone_TxProto_Msg_t OpenDrone_TxProto_Msg = {0};
 /* USER CODE END PV */
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
@@ -99,103 +78,13 @@ int main(void)
     MX_USART1_UART_Init();
     MX_I2C2_Init();
     /* USER CODE BEGIN 2 */
-    PeriphIMU_Init();
-#ifdef USE_SERIAL_DEBUG
-    sprintf((char *)log_buf, "\r\nInit peripheral IMU complete");
-    hw_intf_uart_debug_send(log_buf, 30);
-#endif
-
-    PeriphRadio_Init();
-#ifdef USE_SERIAL_DEBUG
-    sprintf((char *)log_buf, "\r\nInit peripheral RADIO complete");
-    hw_intf_uart_debug_send(log_buf, 32);
-#endif
-
-    PeriphEsc_Init();
-#ifdef USE_SERIAL_DEBUG
-    sprintf((char *)log_buf, "\r\nInit peripheral ESC complete");
-    hw_intf_uart_debug_send(log_buf, 30);
-#endif
-
-    PeriphController_Init();
-#ifdef USE_SERIAL_DEBUG
-    sprintf((char *)log_buf, "\r\nInit peripheral CONTROLLER complete");
-    hw_intf_uart_debug_send(log_buf, 37);
-#endif
+    OpenDrone_FC_Init();
     /* USER CODE END 2 */
     /* Infinite loop */
     /* USER CODE BEGIN WHILE */
     while (1)
     {
-        uint32_t current_time = hw_intf_get_time_us();
-
-        /* Task 500 Hz */
-        if ((current_time - last_time_us[IDX_TASK_500_HZ]) > FREQ_500_HZ_TIME_US)
-        {
-        	PeriphIMU_UpdateAccel();
-        	PeriphIMU_UpdateGyro();
-        	PeriphIMU_UpdateFilter();
-
-#ifdef USE_SERIAL_DEBUG
-            task_freq[IDX_TASK_500_HZ] = TIME_US_TO_FREQ_HZ(current_time - last_time_us[IDX_TASK_500_HZ]);
-#endif
-
-            last_time_us[IDX_TASK_500_HZ] = current_time;
-        }
-
-        /* Task 100 Hz */
-        if ((current_time - last_time_us[IDX_TASK_100_HZ]) > FREQ_100_HZ_TIME_US)
-        {
-            last_time_us[IDX_TASK_100_HZ] = current_time;
-        }
-
-        /* Task 50 Hz */
-        if ((current_time - last_time_us[IDX_TASK_50_HZ]) > FREQ_50_HZ_TIME_US)
-        {
-        	PeriphRadio_Receive((uint8_t *)&OpenDrone_TxProto_Msg);
-
-#ifdef USE_SERIAL_DEBUG
-            task_freq[IDX_TASK_50_HZ] = TIME_US_TO_FREQ_HZ(current_time - last_time_us[IDX_TASK_50_HZ]);
-#endif
-
-            last_time_us[IDX_TASK_50_HZ] = current_time;
-        }
-
-        /* Task 5 Hz */
-        if ((current_time - last_time_us[IDX_TASK_5_HZ]) > FREQ_5_HZ_TIME_US)
-        {
-#ifdef USE_SERIAL_DEBUG
-
-#ifdef PRINT_ANGLE
-            float debug_roll, debug_pitch, debug_yaw;
-            periph_imu_get_angel(&debug_roll, &debug_pitch, &debug_yaw);
-
-            task_freq[IDX_TASK_5_HZ] = TIME_US_TO_FREQ_HZ(current_time - last_time_us[IDX_TASK_5_HZ]);
-
-            sprintf((char *)log_buf, "\r\nroll: %7.4f\t\tpitch: %7.4f\t\tyaw: %7.4f\t", debug_roll, debug_pitch, debug_yaw);
-            hw_intf_uart_debug_send(log_buf, 50);
-
-            sprintf((char *)log_buf, "\r\nTask 500 Hz actual frequency: %d Hz", task_freq[IDX_TASK_500_HZ]);
-            hw_intf_uart_debug_send(log_buf, 45);
-
-            sprintf((char *)log_buf, "\r\nTask 50 Hz actual frequency: %d Hz", task_freq[IDX_TASK_50_HZ]);
-            hw_intf_uart_debug_send(log_buf, 45);
-
-            sprintf((char *)log_buf, "\r\nTask 5 Hz actual frequency: %d Hz", task_freq[IDX_TASK_5_HZ]);
-
-            hw_intf_uart_debug_send(log_buf, 45);
-#endif
-
-            sprintf((char *)log_buf, "\r\nthrottle: %03d \troll: %03d \tpitch: %03d \tyaw: %03d",
-                    OpenDrone_TxProto_Msg.Payload.StabilizerCtrl.throttle,
-                    OpenDrone_TxProto_Msg.Payload.StabilizerCtrl.roll,
-                    OpenDrone_TxProto_Msg.Payload.StabilizerCtrl.pitch,
-                    OpenDrone_TxProto_Msg.Payload.StabilizerCtrl.yaw);
-
-            hw_intf_uart_debug_send(log_buf, 50);
-#endif
-            last_time_us[IDX_TASK_5_HZ] = current_time;
-        }
+        OpenDrone_FC_Main();
         /* USER CODE END WHILE */
         /* USER CODE BEGIN 3 */
     }
